078c4eea166eeb2c9c50b819b5cc6263240aedf3
Raju Khunt RK <rajukhunt15@gmail.com>
2019-11-29 18:36:05 +0530

    code formate

--- src/main/java/ai/jobiak/lambda/JobiakAwsLambdaApplication.java
+++ src/main/java/ai/jobiak/lambda/JobiakAwsLambdaApplication.java
@@ -3,8 +3,6 @@ package ai.jobiak.lambda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 
